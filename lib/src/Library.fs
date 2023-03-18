9751931f69afacb85903b9c9788902e5fa04c546
Noah Hood <hood.noah@icloud.com>
2023-03-18 19:15:53 -0400

    scaffold project template

---
+++ lib/src/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace SubsetSum
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ lib/test/Main.fs
@@ -0,0 +1,6 @@
+﻿module SubsetSumTest
+open Expecto
+
+[<EntryPoint>]
+let main argv =
+    Tests.runTestsInAssembly defaultConfig argv
