0a7eb2177fb17af92c0287d76ce3d2097053818c
Noah Hood <86262760+hoodnoah@users.noreply.github.com>
2023-04-17 19:05:41 +0000

    Pretty print (#9) (#10)
    * implement method for producing a table from labeledValues
    
    * change get_table to take a reference to a slice of values
    
    * update main to utilize improved tabular printing
    
    * update spelling dictionary
    
    * implement pretty-printing with more normalized decimal places
    
    * add spelling check words to dictionary
    
    * add printing of target before printing subset table
    
    * update test_yaml.yml to include decimal places
    
    * update changelog
    
    * fix bug in test case which expected title row to be counted as a row

---
+++ src/pretty_print.rs
@@ -0,0 +1,57 @@
+use crate::find_subsets::LabeledValue;
+use prettytable::{row, Table};
+
+// Finds the maximum number of decimal places in a list of LabeledValue
+fn find_max_num_decimal_places(input: &[&LabeledValue]) -> usize {
+    input
+        .iter()
+        .map(|x| (x.value.fract()).to_string().len())
+        .max()
+        .unwrap()
+        .max(0)
+}
+
+fn format_float(value: f32, num_decimal_places: usize) -> String {
+    format!("{:.*}", num_decimal_places, value)
+}
+
+/// Given a list of LabeledValue, return a prettytable::Table
+pub fn get_table(input: &[&LabeledValue]) -> Table {
+    let decimal_places = find_max_num_decimal_places(input);
+
+    let mut table = Table::new();
+    table.set_titles(row!["LABEL", "VALUE"]);
+    for &item in input {
+        table.add_row(row![item.label, r->format_float(item.value, decimal_places)]);
+    }
+    table
+}
+
+#[cfg(test)]
+mod pretty_print_tests {
+    use super::*;
+    use crate::find_subsets::LabeledValue;
+    use proptest::prelude::*;
+
+    proptest! {
+      #[test]
+      fn returns_table_with_a_row_per_value(input in prop::collection::vec(any::<f32>(), 1..100)) {
+        let labeled_values = input.iter().map(|x| LabeledValue { label: x.to_string(), value: *x }).collect::<Vec<LabeledValue>>();
+        let table = get_table(&labeled_values.iter().collect::<Vec<&LabeledValue>>());
+
+        assert_eq!(input.len(), table.len()); // title row is not included in table.len()
+      }
+    }
+
+    proptest! {
+      #[test]
+      fn is_idempotent(input in prop::collection::vec(any::<f32>(), 1..100)) {
+        let labeled_values = input.iter().map(|x| LabeledValue { label: x.to_string(), value: *x }).collect::<Vec<LabeledValue>>();
+        let input = labeled_values.iter().collect::<Vec<&LabeledValue>>();
+        let table1 = get_table(&input);
+        let table2 = get_table(&input);
+
+        assert_eq!(table1, table2);
+      }
+    }
+}
